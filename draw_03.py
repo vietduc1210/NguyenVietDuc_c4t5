c530e05593823deef946ce46533e6ff106af66c0
Admin <ngvietduc03@gmail.com>
2018-07-26 19:24:50 +0700

    first comit

---
+++ draw_03.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+color("red")
+for i in range(4):
+    right(30)
+    forward(100)
+    left(60)
+    forward(100)
+    left(120)
+    forward(100)
+    left(60)
+    forward(100)
+    right(120)
+mainloop()
